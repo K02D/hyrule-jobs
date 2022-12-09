580561cffa631bf91bd7b4c541eed36317fc2f42
K02D <kirondeb02@gmail.com>
2022-12-09 14:43:05 -0500

    Implement logic to sort jobs based on criteria

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="app">
-    <JobsList :jobs="jobs" />
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    <JobsList :jobs="jobs" :order="order"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import Job from './types/Job';
+import OrderBy from './types/OrderBy';
 import JobsList from './components/JobsList.vue';
 
 export default defineComponent({
@@ -21,13 +29,37 @@ export default defineComponent({
     { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
     { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
   ])
-  return {
-    jobs
+
+  const order = ref<OrderBy>('title'); // can only be one of 3 values specified by the OrderBy tpye
+
+  const handleClick = (orderSelected: OrderBy) => {
+    order.value = orderSelected;
+    // sort based on the order selected
+  }
+  return { // everything that we return can be used the template above
+    jobs,
+    handleClick,
+    order
   }
 }
 });
 </script>
 
 <style>
-
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
 </style>
--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="job-list">
+      <p>Ordered by {{ order }}</p>
         <ul>
-            <li v-for="job in jobs" :key="job.id">
+            <li v-for="job in orderedJobs" :key="job.id">
                 <h2>{{ job.title }} in {{ job.location }}</h2>
                 <div class="salary">
                     <p>{{ job.salary }}</p>
@@ -20,14 +21,28 @@
 
 <script lang="ts">
 import Job from '@/types/Job';
-import { defineComponent, PropType } from 'vue';
+import OrderBy from '@/types/OrderBy';
+import { computed, defineComponent, PropType } from 'vue';
 
     export default defineComponent({
         props: {
             jobs: {
                 required: true,
                 type: Array as PropType<Job[]>
+            },
+            order: {
+              required: true,
+              type: String as PropType<OrderBy>
             }
+        },
+        setup(props) {
+          // computed properties are cached, so they only re-run when the dependencies change
+          const orderedJobs = computed(() => {
+            return [...props.jobs].sort((a: Job, b: Job) => {
+              return a[props.order] > b[props.order] ? 1 : -1
+            })
+          })
+          return { orderedJobs }
         }
     })
 </script>
